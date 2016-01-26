1d867cb46e1a7d822e88b6808b4d7017732d974e
Joshua Snider <sniderboy2005@gmail.com>
2016-01-26 08:54:40 -0500

    Commannd updates

--- com.py
+++ com.py
@@ -4,10 +4,39 @@
 This is a module for bot commands so that bot.py can import them.
 Commands are if statements within functions. The ls tables let bot.py know that the command exists.
 """
+import random
 
-addrls = ['part','join','act','speak','quit','slap','die']
-ls = ['!help','.help','!unflip','.unflip','!flip','.flip','^5']
+addrls = ['part','join','act','speak','quit','slap','die'.decode('utf-8')]
+ls = ['happy','dong','smoak','donger','!help','.help','!unflip','.unflip','!flip','.flip','^5'.decode('utf-8')]
 trusted = 'josh1238!~josh1238@unaffiliated/josh1238'
+donger = ['ヽ〳 ՞ ᗜ ՞ 〵ง'.decode('utf-8'),
+  's( ^ ‿ ^)-b'.decode('utf-8'),
+  '(つ°ヮ°)つ  └⋃┘'.decode('utf-8'),
+  '┬─┬ノ( ◕◡◕ ノ)'.decode('utf-8'),
+  '╰( ◕ ᗜ ◕ )╯'.decode('utf-8'),
+  'ଘ(੭*ˊᵕˋ)੭'.decode('utf-8'),
+  'd–(^ ‿ ^ )z'.decode('utf-8'),
+  '(つ°ヮ°)つ'.decode('utf-8'),
+  '(> ^_^ )>'.decode('utf-8'),
+  '☜(ﾟヮﾟ☜)'.decode('utf-8'),
+  '┗(＾0＾)┓'.decode('utf-8'),
+  '໒( ͡ᵔ ͜ʖ ͡ᵔ )७'.decode('utf-8'),
+  '(ノ°▽°)ノ︵┻━┻'.decode('utf-8'),
+  'ᕕ╏ ͡ ▾ ͡ ╏┐'.decode('utf-8'),
+  '¯\_( ͠° ͟ʖ °͠ )_/¯'.decode('utf-8'),
+  '(ᓄಠ_ಠ)ᓄ'.decode('utf-8'),
+  'ʕง•ᴥ•ʔง'.decode('utf-8'),
+  '(ง’̀-‘́)ง'.decode('utf-8'),
+  '(งಠ_ಠ)ง'.decode('utf-8'),
+  '༼⁰o⁰；༽'.decode('utf-8'),
+  'ԅ⁞ ◑ ₒ ◑ ⁞ᓄ'.decode('utf-8'),
+  '། – _ – །'.decode('utf-8'),
+  '༼つಠ益ಠ༽つ ─=≡ΣO))'.decode('utf-8'),
+  'ヽ(⌐■_■)ノ♪♬'.decode('utf-8'),
+  '♪ヽ( ⌒o⌒)人(⌒-⌒ )v ♪'.decode('utf-8'),
+  '♪O<( ･ ∀ ･ )っ┌iii┐'.decode('utf-8'),
+  '[ ⇀ ‿ ↼ ]'.decode('utf-8'),
+  'ヽ(”`▽´)ﾉ'.decode('utf-8')]
 
 def AddrFuncs(cmd, args, data, conn):
   chan = data['channel']
@@ -34,14 +63,14 @@ def AddrFuncs(cmd, args, data, conn):
       msg = ' '.join(args[3:])
     else:
       msg = ' '.join(args[1:])
-    if to:
+    if 'to' in locals():
       conn.say(msg, chan, to)
     else:
       conn.say(msg, chan)
   elif cmd == 'quit':
     asker = data['sender']
     if asker == trusted:
-      conn.quit('shutting down')
+      conn.quit('goodbye for now')
     else:
       conn.say('┌∩┐(ಠ_ಠ)┌∩┐'.decode('utf-8'), chan)
   elif cmd == 'die':
@@ -51,7 +80,12 @@ def UnAddrFuncs(cmd, args, data, conn):
   chan = data['channel']
   if cmd == '!help' or cmd == '.help':
     conn.say("happypizza doesn't want to help you", chan)
-  if cmd == '!flip' or cmd == '.flip':
+  elif cmd.lower() == 'happy':
+    if args[0].lower() == 'birthday' or args[0].lower() == 'bday':
+      conn.say('♪O<( ･ ∀ ･ )っ┌iii┐'.decode('utf-8'), chan)
+  elif cmd == 'donger' or cmd == 'dong' or cmd == 'smoak':
+    conn.say(random.choice(donger), chan)
+  elif cmd == '!flip' or cmd == '.flip':
     conn.say('(ノ°▽°)ノ︵┻━┻'.decode('utf-8'), chan)
   elif cmd == '!unflip' or cmd == '.unflip':
     conn.say('┬─┬ノ(°▽°ノ)'.decode('utf-8'), chan)
@@ -66,7 +100,7 @@ def OnJoinFuncs(channel, conn):
 
 def OtherJoinFuncs(data, conn):
   if data['nick'] == 'zeezey':
-    conn.say('( ´・ω・)つ──☆✿✿✿✿✿✿ Welcome to the channel zeezey!'.decode('utf-8'), data['channel'])
+    conn.say('( ´・ω・)つ──☆✿✿✿✿✿✿ Welcome President zeezey!'.decode('utf-8'), data['channel'])
 #  else:
 #    conn.say('Hi my name is spacecake', data['channel'])
 
